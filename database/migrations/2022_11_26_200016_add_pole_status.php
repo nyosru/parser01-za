815b05c8fb2fef9bcae753ab3f70e2c3876d9662
Сергей <1@php-cat.com>
2022-11-27 01:44:43 +0500

    добавил значения статуса в таблицу товаров

---
+++ database/migrations/2022_11_26_200016_add_pole_status.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPoleStatus extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('goods', function (Blueprint $table) {
+            $table->set('load-type', ['new', 'loaded', 'full','error'])
+                ->default('new')
+                ->comment('статус загрузки первая и с полной страницы')
+                ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('goods', function (Blueprint $table) {
+            $table->set('load-type', ['new', 'loaded', 'full'])
+                ->default('new')
+                ->comment('статус загрузки первая и с полной страницы')
+                ->change();
+        });
+    }
+}
